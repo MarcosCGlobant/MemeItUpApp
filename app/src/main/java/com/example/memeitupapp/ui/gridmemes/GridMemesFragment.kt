f73666ceacd72d7ad2cc370eebaa5d41400c5a7e
Marcos Cifuentes <marcos.cifuentes@globant.com>
2020-05-12 11:50:26 -0300

    added base MVVM structure and dependencies

---
+++ app/src/main/java/com/example/memeitupapp/ui/gridmemes/GridMemesFragment.kt
@@ -0,0 +1,18 @@
+package com.example.memeitupapp.ui.gridmemes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class GridMemesFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
