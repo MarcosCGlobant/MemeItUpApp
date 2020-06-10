6aa488d591ef449bb39531547700d71c139d942a
benjaglobant <benjaglobant@users.noreply.github.com>
2020-06-10 15:50:31 -0300

    implementing dependency injection layer

---
+++ app/src/main/java/com/example/memeitupapp/di/KoinModules.kt
@@ -0,0 +1,25 @@
+package com.example.memeitupapp.di
+
+import com.example.memeitupapp.ui.contract.GridMemesContract
+import com.example.memeitupapp.ui.contract.ListMemesContract
+import com.example.memeitupapp.ui.contract.MemesDetailsContract
+import com.example.memeitupapp.ui.gridmemes.model.GridMemesModel
+import com.example.memeitupapp.ui.gridmemes.viewmodel.GridMemesViewModel
+import com.example.memeitupapp.ui.listmemes.model.ListMemesModel
+import com.example.memeitupapp.ui.listmemes.viewmodel.ListMemesViewModel
+import com.example.memeitupapp.ui.memedetail.model.MemeDetailModel
+import com.example.memeitupapp.ui.memedetail.viewmodel.MemeDetailViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelsModule = module {
+    viewModel { GridMemesViewModel(get()) }
+    viewModel { ListMemesViewModel(get()) }
+    viewModel { MemeDetailViewModel(get()) }
+}
+
+val modelsModule = module {
+    single<GridMemesContract.Model> { GridMemesModel(get()) }
+    single<ListMemesContract.Model> { ListMemesModel(get()) }
+    single<MemesDetailsContract.Model> { MemeDetailModel(get()) }
+}
