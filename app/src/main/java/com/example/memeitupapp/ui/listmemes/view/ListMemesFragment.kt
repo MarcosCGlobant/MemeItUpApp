65ce17a3964e729ba23402b49b26783e9f8a7aae
benjaglobant <benjaglobant@users.noreply.github.com>
2020-06-22 10:29:07 -0300

    UI improvements

--- app/src/main/java/com/example/memeitupapp/ui/listmemes/view/ListMemesFragment.kt
+++ app/src/main/java/com/example/memeitupapp/ui/listmemes/view/ListMemesFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.memeitupapp.R
 import com.globant.domain.entity.Meme
 import com.example.memeitupapp.ui.adapter.ListMemesAdapter
@@ -15,6 +15,7 @@ import com.example.memeitupapp.ui.memedetail.view.MemeDetailFragment
 import com.example.memeitupapp.util.Data
 import com.example.memeitupapp.util.Event
 import com.example.memeitupapp.util.Status
+import com.example.memeitupapp.util.getColumnsByOrientation
 import kotlinx.android.synthetic.main.layout_fragment_list_memes.layout_fragment_list_memes_progress_bar
 import kotlinx.android.synthetic.main.layout_fragment_list_memes.layout_fragment_list_memes_recycler_view
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -24,12 +25,22 @@ class ListMemesFragment : Fragment() {
     private val listMemesViewModel by viewModel<ListMemesViewModel>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.layout_fragment_list_memes, container, false)
+            inflater.inflate(R.layout.layout_fragment_list_memes, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         listMemesViewModel.getLiveData().observe(::getLifecycle, ::updateUI)
+        listMemesViewModel.fetchMemes()
+    }
+
+    override fun onPause() {
+        layout_fragment_list_memes_recycler_view.visibility = View.GONE
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
         listMemesViewModel.fetchMemes()
     }
 
@@ -63,8 +74,17 @@ class ListMemesFragment : Fragment() {
                 memeFragment.show(childFragmentManager, getString(R.string.tag))
             }
             listMemesAdapter.submitList(memes)
-            layout_fragment_list_memes_recycler_view.layoutManager = LinearLayoutManager(context)
-            layout_fragment_list_memes_recycler_view.adapter = listMemesAdapter
+            layout_fragment_list_memes_recycler_view.apply {
+                layoutManager =
+                        GridLayoutManager(context, resources.configuration.getColumnsByOrientation(COLUMNS_PORTRAIT, COLUMNS_LANDSCAPE))
+                adapter = listMemesAdapter
+                visibility = View.VISIBLE
+            }
         }
+    }
+
+    companion object {
+        private const val COLUMNS_PORTRAIT = 1
+        private const val COLUMNS_LANDSCAPE = 2
     }
 }
---
+++ app/src/main/java/com/example/memeitupapp/util/GetColumnsByOrientationFunction.kt
@@ -0,0 +1,12 @@
+package com.example.memeitupapp.util
+
+import android.content.res.Configuration
+
+fun Configuration.getColumnsByOrientation(portrait: Int, landscape: Int): Int =
+        orientation.let {
+            return when (it) {
+                Configuration.ORIENTATION_PORTRAIT -> portrait
+                Configuration.ORIENTATION_LANDSCAPE -> landscape
+                else -> portrait
+            }
+        }
